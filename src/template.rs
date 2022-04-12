4bb199880a3debce777139c0e787995e0488ba3f
cympfh <cympfh@gmail.com>
2022-04-12 23:05:26 +0900

    -A,-B,-H,-C

--- src/template.rs
+++ src/template.rs
@@ -2,12 +2,36 @@ use handlebars::{Handlebars, RenderError};
 use serde::Serialize;
 
 #[derive(Serialize)]
-struct Context {
+pub struct Context {
     title: String,
     body: String,
+    css: Vec<String>,
+    headers: Vec<String>,
+    befores: Vec<String>,
+    afters: Vec<String>,
 }
 
-pub fn simple(title: String, body: String) -> Result<String, RenderError> {
+impl Context {
+    pub fn new(
+        title: String,
+        body: String,
+        css: Vec<String>,
+        headers: Vec<String>,
+        befores: Vec<String>,
+        afters: Vec<String>,
+    ) -> Self {
+        Self {
+            title,
+            body,
+            css,
+            headers,
+            befores,
+            afters,
+        }
+    }
+}
+
+pub fn simple(context: Context) -> Result<String, RenderError> {
     const TEMPLATE: &str = r#"<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -20,15 +44,20 @@ pub fn simple(title: String, body: String) -> Result<String, RenderError> {
   <link href="https://unpkg.com/prismjs@1.x.0/themes/prism.css" rel="stylesheet" />
   <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
   <script id="MathJax-script" async src="https://unpkg.com/mathjax@3/es5/tex-mml-chtml.js"></script>
+  {{#each css}}
+  <link href="{{this}}" rel="stylesheet" />
+  {{/each}}
+  {{#each headers}}{{{this}}}{{/each}}
 </head>
 <body>
+  {{#each befores}}{{{this}}}{{/each}}
 {{{body}}}
+  {{#each afters}}{{{this}}}{{/each}}
   <script src="https://cympfh.cc/resources/js/youtube.js"></script>
   <script src="https://unpkg.com/prismjs@v1.x/components/prism-core.min.js"></script>
   <script src="https://unpkg.com/prismjs@v1.x/plugins/autoloader/prism-autoloader.min.js"></script>
 </body>
 </html>"#;
     let reg = Handlebars::new();
-    let context = Context { title, body };
     reg.render_template(TEMPLATE, &context)
 }
